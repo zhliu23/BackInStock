0aff129b97d3c734a02856e388716eb0c1dcc57a
Zhen Liu <zhliu23@gmail.com>
2017-04-24 03:47:23 -0700

    Changed new entry method, added simple network method

--- app/src/main/java/com/example/zhen/backinstock/controller/ItemArrayAdapter.java
+++ app/src/main/java/com/example/zhen/backinstock/controller/ItemArrayAdapter.java
@@ -1,4 +1,4 @@
-package com.example.zhen.backinstock.model;
+package com.example.zhen.backinstock.controller;
 
 import android.app.Activity;
 import android.content.Context;
@@ -7,11 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.zhen.backinstock.R;
+import com.example.zhen.backinstock.model.Item;
 
 import java.util.List;
 
@@ -21,28 +20,30 @@ import java.util.List;
  * Created by Zhen on 4/10/2017.
  */
 
-public class ItemAdapter extends ArrayAdapter<Item> {
+public class ItemArrayAdapter extends ArrayAdapter<Item> {
 
-    private List<Item> itemList;
     private Context context;
+    private int layoutResource;
+    private List<Item> itemList;
 
-    public ItemAdapter(Context context, List<Item> itemList) {
-        super(context, R.layout.fragment_row, itemList);
+    public ItemArrayAdapter(Context context, int resource, List<Item> itemList) {
+        super(context, resource, itemList);
         this.context = context;
+        this.layoutResource = resource;
         this.itemList = itemList;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-        convertView = inflater.inflate(R.layout.fragment_row, parent, false);
+        View view = inflater.inflate(R.layout.listview_item, parent, false);
 
-        TextView nameText = (TextView) convertView.findViewById(R.id.nameText);
-        TextView priceText = (TextView) convertView.findViewById(R.id.priceText);
-        TextView statusText = (TextView) convertView.findViewById(R.id.statusText);
+        TextView nameText = (TextView) view.findViewById(R.id.nameText);
+        TextView priceText = (TextView) view.findViewById(R.id.priceText);
+        TextView statusText = (TextView) view.findViewById(R.id.statusText);
 
-        //ImageView imageView = (ImageView) convertView.findViewById(R.id.itemImage);
-        //CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
+        //ImageView imageView = (ImageView) view.findViewById(R.id.itemImage);
+        //CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox);
 
         Item item = itemList.get(position);
 
@@ -52,6 +53,6 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         statusText.setTextColor(item.isInStock() ? Color.GREEN : Color.RED);
         //itemList.get(position).setChecked((checkBox.isChecked()) ? true : false);
 
-        return convertView;
+        return view;
     }
 }
--- app/src/main/java/com/example/zhen/backinstock/controller/MainActivity.java
+++ app/src/main/java/com/example/zhen/backinstock/controller/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.zhen.backinstock.controller;
 
-import android.content.Intent;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.zhen.backinstock.R;
-import com.example.zhen.backinstock.model.ItemAdapter;
 import com.example.zhen.backinstock.model.Item;
-import com.example.zhen.backinstock.view.NewEntryActivity;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,30 +30,62 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton addFAB;
 
     private ListView listView;
-    private ItemAdapter adapter;
-
-    private List<Item> itemList = new ArrayList<>();
+    private ItemArrayAdapter itemAdapter;
+    private List<Item> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initItemList();
+
         //init all variables for ListView
         listView = (ListView) findViewById(R.id.listView);
-        adapter = new ItemAdapter(this, itemList);
-        listView.setAdapter(adapter);
-        itemList.add(new Item("", "Nintendo Switch", 299.99, false));
-        itemList.add(new Item("", "Fire Emblem Echoes", 39.99, true));
+        itemAdapter = new ItemArrayAdapter(this, R.layout.listview_item, itemList);
+        listView.setAdapter(itemAdapter);
 
         //Floating Action Button
         addFAB = (FloatingActionButton) findViewById(R.id.addFAB);
         addFAB.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, NewEntryActivity.class));
+                //startActivity(new Intent(MainActivity.this, NewEntryActivity.class));
+                AlertDialog.Builder newEntryBuilder = new AlertDialog.Builder(MainActivity.this);
+                newEntryBuilder.setTitle("New Entry");
+
+                final EditText urlText = new EditText(MainActivity.this);
+                urlText.setInputType(InputType.TYPE_TEXT_VARIATION_WEB_EDIT_TEXT);
+                urlText.setHint("URL");
+
+                newEntryBuilder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(!urlText.getText().toString().matches(""))
+                            (new parseURL()).execute(new String[]{urlText.getText().toString()});
+                        else
+                            dialog.cancel();
+                    }
+                });
+                newEntryBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                newEntryBuilder.setView(urlText);
+                newEntryBuilder.show();
             }
         });
+    }
+
+    private void initItemList() {
+        itemList = new ArrayList<>();
+        itemList.add(new Item("", "Fire Emblem", 59.99, false));
+        itemList.add(new Item("", "Nintendo Switch", 299.99, true));
+        itemList.add(new Item("", "Final Fantasy", 59.99, true));
+
     }
 
     @Override
@@ -66,6 +106,32 @@ public class MainActivity extends AppCompatActivity {
 //                return true;
             default:
                 return true;
+        }
+    }
+
+    private class parseURL extends AsyncTask<String, Void, String> {
+
+        Item newItem;
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                Document doc  = Jsoup.connect(strings[0]).get();
+                String title = doc.title();
+                newItem = new Item("", title, 0.0, false);
+
+            }
+            catch(Throwable t) {
+                t.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            itemAdapter.add(newItem);
+            itemAdapter.notifyDataSetChanged();
         }
     }
 }
